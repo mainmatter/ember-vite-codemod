f658b8860e338178cc27be4dcee9c0f7b3a7554b
Marine Dunstetter <bluecut.official@gmail.com>
2025-02-18 18:37:39 +0100

    feat: modify ember-cli-build.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import addMissingFiles from './lib/addMissingFiles.js';
 import moveIndex from './lib/moveIndex.js';
+import modifyFiles from './lib/modifyFiles.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const pkg = JSON.parse(await readFile(join(__dirname, 'package.json'), 'utf8'));
@@ -37,5 +38,6 @@ program.parse();
 // await ensureV2Addons();
 await addMissingFiles();
 await moveIndex();
+await modifyFiles();
 // TODO
 // await updatePackageJson();
