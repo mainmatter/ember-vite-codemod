8820e21d283f1bb4daf2645e344a09b9301e82f1
Marine Dunstetter <bluecut.official@gmail.com>
2025-02-20 11:48:38 +0100

    feat: iterate on files transform modify testem.js modify eslint.config.mjs setup index
    feat: setup index

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ import { readFile } from 'node:fs/promises';
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import addMissingFiles from './lib/addMissingFiles.js';
-import ensureV2Addons from './lib/ensureV2Addons.js'
-import moveIndex from './lib/moveIndex.js';
+import ensureV2Addons from './lib/ensureV2Addons.js';
 import modifyFiles from './lib/modifyFiles.js';
+import moveIndex from './lib/moveIndex.js';
+import updatePackageJson from './lib/updatePackageJson.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const pkg = JSON.parse(await readFile(join(__dirname, 'package.json'), 'utf8'));
@@ -17,9 +18,9 @@ program.parse();
 
 const options = program.opts();
 
+// Tasks order is important
 await ensureV2Addons(options.skipV2Addon);
 await addMissingFiles();
 await moveIndex();
 await modifyFiles();
-// TODO
-// await updatePackageJson();
+await updatePackageJson();
