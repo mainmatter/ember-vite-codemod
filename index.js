d6195bec6585e7f0d41bd23f26f00342a3bc1f4f
Marine Dunstetter <bluecut.official@gmail.com>
2025-02-21 14:30:23 +0100

    feat: iterate on add missing files and transforms - add app/config/env - implement project detection - modify tests/index.html - add placeholders for app/app.js and tests/test-helper.js

--- index.js
+++ index.js
@@ -12,15 +12,19 @@ import updatePackageJson from './lib/updatePackageJson.js';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const pkg = JSON.parse(await readFile(join(__dirname, 'package.json'), 'utf8'));
 
-program.option('--skip-v2-addon').version(pkg.version);
+program
+  .option('--skip-v2-addon', 'pursue the execution even when an upgradable v1 addon is detected', false)
+  .option('--ts', 'indicate the app to migrate uses TypeScript', false)
+  .version(pkg.version);
 
 program.parse();
 
 const options = program.opts();
+const projectType = options.ts ? 'ts' : 'js';
 
 // Tasks order is important
 await ensureV2Addons(options.skipV2Addon);
-await addMissingFiles();
+await addMissingFiles({ projectType });
 await moveIndex();
 await modifyFiles();
 await updatePackageJson();
