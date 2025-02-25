ccb27fed8eae92d5f7f2365ebeefaf1563e41bd9
Marine Dunstetter <bluecut.official@gmail.com>
2025-02-25 13:47:45 +0100

    feat: add ensure-no-unsupported-dep

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ import { readFile } from 'node:fs/promises';
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import addMissingFiles from './lib/tasks/add-missing-files.js';
+import ensureNoUnsupportedDeps from './lib/tasks/ensure-no-unsupported-deps.js';
 import ensureV2Addons from './lib/tasks/ensure-v2-addons.js';
 import moveIndex from './lib/tasks/move-index.js';
 import transformFiles from './lib/tasks/transform-files.js';
 import updatePackageJson from './lib/tasks/update-package-json.js';
+
+// Continue if git is clean
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const pkg = JSON.parse(await readFile(join(__dirname, 'package.json'), 'utf8'));
@@ -23,8 +26,11 @@ const options = program.opts();
 const projectType = options.ts ? 'ts' : 'js';
 
 // Tasks order is important
+await ensureNoUnsupportedDeps();
 await ensureV2Addons(options.skipV2Addon);
 await addMissingFiles({ projectType });
 await moveIndex();
 await transformFiles();
 await updatePackageJson();
+
+console.log('All set! Re-install the app dependencies then run your linter');
