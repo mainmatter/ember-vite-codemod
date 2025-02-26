801a01ee26352445c176ce65f6a73a1c32a59c90
Chris Manson <chris@manson.ie>
2025-02-26 14:19:07 +0000

    enable CI using upgrade script

--- tests/all-versions.test.js
+++ tests/all-versions.test.js
@@ -7,8 +7,8 @@ import { packageUp } from 'package-up';
 
 const require = createRequire(import.meta.url);
 
-// import { fileURLToPath } from 'node:url';
-// const __dirname = dirname(fileURLToPath(import.meta.url));
+import { fileURLToPath } from 'node:url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const testVersions = [
   ['ember-cli-3.28'],
@@ -52,18 +52,23 @@ describe('Test on all Ember versions', function () {
       expect(stdout).to.include('# pass  1');
       expect(stdout).to.include('# fail  0');
 
-      // TODO uncomment this when it's actually working
-      // const updateScriptPath = join(__dirname, '../index.js');
-      // await execaNode({cwd})`${updateScriptPath}`;
+      // this prevents the script from erroring because of this
+      await execa({ cwd })`pnpm uninstall ember-fetch`;
 
-      // // TODO move this to just be pnpm install assuming that the script would add vite
-      // await execa({cwd})`pnpm i vite`;
+      const updateScriptPath = join(__dirname, '../index.js');
+      await execaNode({
+        cwd,
+        stdio: 'inherit',
+      })`${updateScriptPath} --skip-v2-addon`;
 
-      // await execa({cwd})`./node_modules/.bin/vite build`;
-      // let result = await execa({cwd})`${cliPath} test --path dist`;
+      await execa({ cwd, stdio: 'inherit' })`pnpm i`;
 
-      // expect(result.stdout).to.include('# pass  1');
-      // expect(result.stdout).to.include('# fail  0');
+      await execa({ cwd, stdio: 'inherit' })`./node_modules/.bin/vite build`;
+      let result = await execa({ cwd })`${cliPath} test`;
+      console.log(result.stdout);
+
+      expect(result.stdout).to.include('# pass  1');
+      expect(result.stdout).to.include('# fail  0');
     });
   }
 });
