b5119a7bb898dfb215b105a84633ddb512f2baea
Chris Manson <chris@manson.ie>
2025-03-14 21:29:56 +0000

    enable testing for ember 4.12

--- tests/test-helpers.js
+++ tests/test-helpers.js
@@ -104,9 +104,9 @@ export async function testWithTestem(cwd, expect, testemPort) {
 
 export const testVersions = [
   // ['ember-cli-3.28'],
-  // ['ember-cli-4.12'],
   // ['ember-cli-4.4'],
   // ['ember-cli-4.8'],
+  ['ember-cli-4.12'],
   // // test helpers seems to be broken for most ember versions 😭
   ['ember-cli-5.4', ['@ember/test-helpers@latest']],
   ['ember-cli-5.8', ['@ember/test-helpers@latest']],
