526c1a8d19882d659f4ece794e7c2f78dd635e9a
Chris Manson <chris@manson.ie>
2025-03-16 22:48:51 +0000

    enable testing for 3.28

--- tests/test-helpers.js
+++ tests/test-helpers.js
@@ -125,7 +125,10 @@ export async function testWithTestem(cwd, expect, testemPort) {
 }
 
 export const testVersions = [
-  // ['ember-cli-3.28'],
+  [
+    'ember-cli-3.28',
+    ['ember-data@^5.3.0', 'ember-inflector', 'ember-cli@~4.12.0'],
+  ],
   [
     'ember-cli-4.4',
     ['ember-data@^5.3.0', 'ember-inflector', 'ember-cli@~4.12.0'],
